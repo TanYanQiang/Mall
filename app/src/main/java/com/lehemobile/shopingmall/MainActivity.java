6665719e41afda928ad5ffd0d2766d673a71f8d1
tanyanqiang <tanyanqiang@51yche.com>
2016-06-28 15:14:42 +0800

    bitmapTransform Glide

--- app/src/main/java/com/lehemobile/shopingmall/MainActivity.java
+++ app/src/main/java/com/lehemobile/shopingmall/MainActivity.java
@@ -3,14 +3,24 @@ package com.lehemobile.shopingmall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.lehemobile.shopingmall.api.TestApi;
 import com.lehemobile.shopingmall.api.base.AppErrorListener;
 import com.lehemobile.shopingmall.utils.VolleyHelper;
 import com.orhanobut.logger.Logger;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
+import jp.wasabeef.glide.transformations.ColorFilterTransformation;
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
+import jp.wasabeef.glide.transformations.CropSquareTransformation;
+import jp.wasabeef.glide.transformations.CropTransformation;
+import jp.wasabeef.glide.transformations.GrayscaleTransformation;
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
                 test();
             }
         });
+
+        Glide.with(this).load("http://s.cn.bing.net/az/hprichbg/rb/CallunaVulgaris_ZH-CN11090416298_1920x1080.jpg")
+                .bitmapTransform(
+                        new CropCircleTransformation(this))
+                .placeholder(R.mipmap.ic_launcher)
+                .into((ImageView) findViewById(R.id.image));
     }
 
     private void test() {
