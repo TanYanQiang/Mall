c548862cc6d9a8af9fa6c33092d00e527efc8187
tanyanqiang <tanyanqiang@51yche.com>
2016-08-02 17:49:42 +0800

    收藏UI

---
+++ app/src/main/java/com/lehemobile/shopingmall/ui/common/ChooseCityActivity.java
@@ -0,0 +1,10 @@
+package com.lehemobile.shopingmall.ui.common;
+
+import com.lehemobile.shopingmall.ui.BaseActivity;
+
+/**
+ * 选择城市
+ * Created by tanyq on 2/8/16.
+ */
+public class ChooseCityActivity extends BaseActivity {
+}
