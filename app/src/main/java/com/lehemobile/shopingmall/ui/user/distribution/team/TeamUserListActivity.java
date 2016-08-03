c2b53defec6b00273ee4988ac991252c31438acf
tanyanqiang <tanyanqiang@51yche.com>
2016-08-03 10:30:59 +0800

    我的小伙伴， 实现本地搜索

--- app/src/main/java/com/lehemobile/shopingmall/ui/user/distribution/team/TeamUserListActivity.java
+++ app/src/main/java/com/lehemobile/shopingmall/ui/user/distribution/team/TeamUserListActivity.java
@@ -1,15 +1,22 @@
 package com.lehemobile.shopingmall.ui.user.distribution.team;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.EditText;
 import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 import com.lehemobile.shopingmall.R;
 import com.lehemobile.shopingmall.model.User;
 import com.lehemobile.shopingmall.session.TeamUserSession;
 import com.lehemobile.shopingmall.ui.BaseActivity;
+import com.orhanobut.logger.Logger;
+import com.tgh.devkit.core.utils.TextWatcherAdapter;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -25,11 +32,79 @@ import java.util.List;
 public class TeamUserListActivity extends BaseActivity {
     @ViewById
     ExpandableListView listView;
+    @ViewById
+    EditText searchEdit;
+
     private List<TeamUserSession> data;
+    private TeamUserAdapter teamUserAdapter;
+
 
     @AfterViews
     void init() {
+        initSearch();
+
         loadData();
+    }
+
+    private void initSearch() {
+        searchEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                switch (i) {
+                    case EditorInfo.IME_ACTION_SEARCH:
+                        doSearch();
+                        break;
+                }
+                return false;
+            }
+        });
+        searchEdit.addTextChangedListener(new TextWatcherAdapter() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                doSearch();
+            }
+        });
+    }
+
+    private void doSearch() {
+        if (data == null || data.isEmpty()) return;
+
+        String keyword = getInputText(searchEdit);
+        if (TextUtils.isEmpty(keyword)) {
+            teamUserAdapter.setData(data);
+        } else {
+            List<TeamUserSession> searchData = searchByName(keyword);
+            teamUserAdapter.setData(searchData);
+        }
+
+    }
+
+    private List<TeamUserSession> searchByName(String keyword) {
+
+        List<TeamUserSession> data = new ArrayList<>();
+        if (teamUserAdapter != null) {
+            List<TeamUserSession> sourceData = teamUserAdapter.getData();
+            for (int i = 0; i < sourceData.size(); i++) {
+                TeamUserSession session = sourceData.get(i);
+                List<User> users = session.getUsers();
+
+                List<User> searchUser = new ArrayList<>();
+
+                for (int j = 0; j < users.size(); j++) {
+                    User user = users.get(j);
+                    String nick = user.getNick();
+                    if (nick.contains(keyword)) {
+                        Logger.i("search %s contains %s", keyword, nick);
+                        searchUser.add(user);
+                    }
+                }
+                if (!searchUser.isEmpty()) {
+                    session.setUsers(searchUser);
+                    data.add(session);
+                }
+            }
+        }
+        return data;
     }
 
     private void loadData() {
@@ -41,7 +116,7 @@ public class TeamUserListActivity extends BaseActivity {
         List<User> user1 = new ArrayList<>();
         for (int i = 0; i < 2; i++) {
             User user = new User();
-            user.setNick("小丸子");
+            user.setNick("小丸子"+i);
             user.setAvatar("http://img5.imgtn.bdimg.com/it/u=1998714244,2538311333&fm=206&gp=0.jpg");
             user1.add(user);
         }
@@ -55,7 +130,7 @@ public class TeamUserListActivity extends BaseActivity {
         List<User> user2 = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             User user = new User();
-            user.setNick("流川枫");
+            user.setNick("流川枫"+i);
             user.setAvatar("http://img3.imgtn.bdimg.com/it/u=3681911204,887928701&fm=206&gp=0.jpg");
             user2.add(user);
         }
@@ -68,7 +143,7 @@ public class TeamUserListActivity extends BaseActivity {
         List<User> user3 = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
             User user = new User();
-            user.setNick("樱木花道");
+            user.setNick("樱木花道"+i);
             user.setAvatar("http://img4.imgtn.bdimg.com/it/u=3821141571,2783513459&fm=206&gp=0.jpg");
             user3.add(user);
         }
@@ -82,7 +157,7 @@ public class TeamUserListActivity extends BaseActivity {
         List<User> user4 = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             User user = new User();
-            user.setNick("花伦");
+            user.setNick("花伦"+i);
             user.setAvatar("http://img4.imgtn.bdimg.com/it/u=1685028048,2784143540&fm=21&gp=0.jpg");
             user4.add(user);
         }
@@ -95,7 +170,7 @@ public class TeamUserListActivity extends BaseActivity {
 
     private void updateUI() {
         dismissLoading();
-        TeamUserAdapter teamUserAdapter = new TeamUserAdapter(this, data);
+        teamUserAdapter = new TeamUserAdapter(this, data);
         listView.setAdapter(teamUserAdapter);
         listView.expandGroup(0);
         listView.setGroupIndicator(null);
@@ -104,6 +179,15 @@ public class TeamUserListActivity extends BaseActivity {
     class TeamUserAdapter extends BaseExpandableListAdapter {
         private List<TeamUserSession> data;
         private Context context;
+
+        public List<TeamUserSession> getData() {
+            return data;
+        }
+
+        public void setData(List<TeamUserSession> data) {
+            this.data = data;
+            notifyDataSetChanged();
+        }
 
         public TeamUserAdapter(Context context, List<TeamUserSession> data) {
             this.context = context;
